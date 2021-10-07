c1236d49912b7735814d2a0e5c6a7b60aaaeb7d7
hPixal <nahuel.j.gareis@protonmail.com>
2021-10-07 01:10:39 -0300

    funciona distancia.cpp

--- gtp2/distancia.cpp
+++ gtp2/distancia.cpp
@@ -13,27 +13,55 @@ A partir de allí la capan >= 2 está formada por los vecinos de los vértices d
 la capa n−1 (esdecir la adyacencia de la capa) pero que no están en las capas 
 anteriores (en realidad basta converificar que no estén en las capas n−1 ni n−2). Notar que los v ́ertices en la capanseencuentran a una distanciandel v ́ertices de partida 
 */
-void getTags(graph::iterator &mother,map<int,bool> &visited, map<int,int> &tags,int &tagcost){
+
+void getTags(graph &g,graph::iterator &mother,map<int,bool> &visited, map<int,int> &tags,int tagcost){
     auto it = mother->second.begin();
 
     while(it != mother->second.end()){
-        if (visited.find(*it) != visited.end())
+        if (visited.find(*it) == visited.end())
         {
-            visited[*it] = true;
+            visited[*it] = false;
             tags[*it] = tagcost;
         }
+        ++it;
     }
+
+    visited[mother->first] = true;
     tagcost++;
+
+    it = mother->second.begin();
+    while(it != mother->second.end()){
+        auto newmtr = g.find(*it);
+        if(visited[newmtr->first] == false)
+            getTags(g,newmtr,visited,tags,tagcost);
+        ++it;
+    }
 }
 
 map<int,int> fetchDistance(graph g, int mother){
     map<int,bool> visited; map<int,int> tags;
+    visited[mother] = true; tags[mother] = 0;
     int tagcost = 1;
-
-    
+    auto aux = g.find(mother);
+    getTags(g,aux,visited,tags,tagcost);
+    return tags;
 }
 
 int main(){
+    graph G={
+		{0,{1}},
+	{1,{0,3,4}},
+		{2,{3}},
+	{3,{1,2}},
+		{4,{1}}
+	};
+
+    map<int,int> tags = fetchDistance(G,0);
+    cout << "( ";
+    for(auto x : tags){
+        cout << "{"<<x.first << " " << x.second <<"} ";
+    }
+    cout << ") " << endl;
 
     return 0;
 }
